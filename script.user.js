b3501de22c5b662aceff701b050c2adead202b12
Inevitabby 🐶 <161525738+Inevitabby@users.noreply.github.com>
2025-04-17 03:09:28 +0000

    add: hideFiletype and actions ("remove", "mark", "none")
    - Can now whitelist for wanted filetypes (e.g., .puz)
    - Simplified CONFIG (decoupled function mapping)
      - Now everything should be self-explanatory
    - Replaced `delete <- true/false` with actions:
      - "remove", "mark", and "none"
    - Added hipster comments

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Inevitabby/DailyCrosswordLinks-Filtering/raw/refs/heads/main/script.user.js
 // @match       https://dailycrosswordlinks.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Inevitabby
 // @description Hides links from Daily Crossword Links (e.g., subscription-only, appstore only, etc.)
 // ==/UserScript==
@@ -11,25 +11,31 @@
   "use strict";
   const CONFIG = {
     hidePaid: {
-      enabled: true,
-      test: isPaid,
+      action: "remove", // valid actions: "remove", "mark", "none"
     },
     hideApp: {
-      enabled: true,
-      test: isApp,
+      action: "remove",
     },
     hideCryptic: {
-      enabled: true,
-      test: isCryptic,
+      action: "remove",
+    },
+    hideFiletype: {
+      action: "mark",
+      whitelist: [ ".puz" ]
     },
   };
-  // Returns if entry requires subscription / purchase
-  function isPaid(elem) {
+
+  // ===============================
+  // === Entry Testing Functions ===
+  // ===============================
+
+  // Returns if entry requires subscription or purchase
+  function hidePaid(elem) {
     const strong = elem.querySelector("strong");
     return strong && strong.textContent.includes(": ($)");
   }
-  // Returns if entry requires appstore, exclusively
-  function isApp(elem) {
+  // Returns if entry requires appstore (exclusively)
+  function hideApp(elem) {
     const links = elem.querySelectorAll("a");
     return links.length > 0 && Array.from(links).every(link =>
       link.href.startsWith("https://play.google.com") ||
@@ -37,10 +43,28 @@
     );
   }
   // Returns if entry is a cryptic
-  function isCryptic(elem) {
+  function hideCryptic(elem) {
     const strong = elem.querySelector("strong");
     return strong && strong.textContent.includes("Cryptic");
   }
+  // Returns if entry lacks wanted filetypes
+  function hideFiletype(elem) {
+    const em = elem.querySelector("em");
+    if (em === null) return true;
+    return !CONFIG.hideFiletype.whitelist.some((ft) => em.textContent.includes(ft));
+  }
+  // Local function map
+  const MAP = {
+    hidePaid,
+    hideApp,
+    hideCryptic,
+    hideFiletype,
+  };
+
+  // ====================================
+  // === Entry Modification Functions ===
+  // ====================================
+
   // Delete an entry
   function remove(elem) {
     while (elem) {
@@ -50,10 +74,30 @@
       elem = next;
     }
   }
+  // Mark an entry
+  function mark(elem) {
+    while (elem) {
+      const next = elem.nextSibling;
+      if (elem.style) elem.style.opacity = "0.5";
+      if (elem.nodeName === "BR") break;
+      elem = next;
+    }
+  }
+
+  // ============================
+  // === Iterate over Entries ===
+  // ============================
+
   // Scan through all entries
   const entries = document.querySelectorAll("span.fetched, span.unfetched");
   entries.forEach((entry) => {
-    const match = Object.values(CONFIG).find(({ enabled, test }) => enabled && test(entry));
-    if (match) remove(entry);
+    const matchKey = Object.keys(CONFIG).find((key) => {
+      return MAP[key](entry);
+    });
+    if (!matchKey) return;
+    const { action } = CONFIG[matchKey];
+    if (action === "remove") remove(entry);
+    if (action === "mark") mark(entry);
   });
+
 })();
